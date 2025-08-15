7f93d4e04855327192dc131da9a46ed8466235ed
uvlad7 <uvlad7@gmail.com>
2025-08-16 02:28:12 +0300

    WIP

--- ext/simdjson/simdjson_ruby.cpp
+++ ext/simdjson/simdjson_ruby.cpp
@@ -66,6 +66,13 @@ static VALUE rb_simdjson_parse(VALUE self, VALUE arg) {
     return Qnil;
 }
 
+static inline VALUE to_ruby(double value) { return DBL2NUM(value); }
+
+static inline VALUE to_ruby(std::string_view value) {
+    return rb_str_new(value.data(), value.size());
+}
+
+template <typename T>
 static VALUE rb_simdjson_dig(VALUE self, VALUE arg) {
     Check_Type(arg, T_STRING);
 
@@ -73,11 +80,12 @@ static VALUE rb_simdjson_dig(VALUE self, VALUE arg) {
     padded_string str(RSTRING_PTR(arg), RSTRING_LEN(arg));
     ondemand::document doc = parser.iterate(str);
     // ondemand API doesn't have type-blind dom::element analogs
-    double value;
+    // You first declare the variable of the appropriate type (double, uint64_t, int64_t, bool, ondemand::object and ondemand::array)
+    T value;
     auto error = doc["str"]["123"]["abc"].get(value);
 
     if (error == SUCCESS) {
-        return DBL2NUM(double(value));
+        return to_ruby(value);
     }
     // TODO better error handling
     rb_raise(rb_eSimdjsonParseError, "parse error");
@@ -90,6 +98,7 @@ void Init_simdjson(void) {
     rb_mSimdjson = rb_define_module("Simdjson");
     rb_eSimdjsonParseError = rb_define_class_under(rb_mSimdjson, "ParseError", rb_eStandardError);
     rb_define_module_function(rb_mSimdjson, "parse", reinterpret_cast<VALUE (*)(...)>(rb_simdjson_parse), 1);
-    rb_define_module_function(rb_mSimdjson, "dig", reinterpret_cast<VALUE (*)(...)>(rb_simdjson_dig), 1);
+    rb_define_module_function(rb_mSimdjson, "dig_double", reinterpret_cast<VALUE (*)(...)>(rb_simdjson_dig<double>), 1);
+    rb_define_module_function(rb_mSimdjson, "dig_string", reinterpret_cast<VALUE (*)(...)>(rb_simdjson_dig<std::string_view>), 1);
 }
 }
